9d972f77f867c2fa24c00a40c49489c76b457ffb
sai nagesh <sainagesh632@gamil.com>
2025-12-28 15:50:57 +0530

    Create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,9 @@
-export let cart = JSON.parse(localStorage.getItem('cart'))
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage(){
+  cart = JSON.parse(localStorage.getItem('cart'))
 
 if(!cart){
   cart = [{
@@ -11,6 +16,7 @@ if(!cart){
   quantity : 1,
   deliveryOptionId: '2'
 }]; 
+}
 }
 
 
